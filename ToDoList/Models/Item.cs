1b1acd3e10d6602d2eb680782c246bab05f13dd6
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-05-11 08:23:00 -0700

    Initial copy of branch 33 of epicodus project

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,153 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    private string _description;
+    private int _id;
+
+    public Item (string description, int id = 0)
+    {
+      _description = description;
+      _id = id;
+    }
+
+    public string GetDescription()
+    {
+      return _description;
+    }
+
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public static List<Item> GetAll()
+    {
+      List<Item> allItems = new List<Item> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;";
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int itemId = rdr.GetInt32(0);
+        string itemDescription = rdr.GetString(1);
+        Item newItem = new Item(itemDescription, itemId);
+        allItems.Add(newItem);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allItems;
+    }
+
+    public static void ClearAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM items;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+       conn.Dispose();
+      }
+    }
+
+    public static Item Find(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items WHERE id = (@searchId);";
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = id;
+      cmd.Parameters.Add(searchId);
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      int itemId = 0;
+      string itemName = "";
+      while(rdr.Read())
+      {
+        itemId = rdr.GetInt32(0);
+        itemName = rdr.GetString(1);
+      }
+      Item newItem = new Item(itemName, itemId);
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return newItem;
+    }
+
+    public override bool Equals(System.Object otherItem)
+    {
+      if (!(otherItem is Item))
+      {
+        return false;
+      }
+      else
+      {
+         Item newItem = (Item) otherItem;
+         bool idEquality = this.GetId() == newItem.GetId();
+         bool descriptionEquality = this.GetDescription() == newItem.GetDescription();
+         return (idEquality && descriptionEquality);
+       }
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO items (description) VALUES (@description);";
+      MySqlParameter description = new MySqlParameter();
+      description.ParameterName = "@description";
+      description.Value = this._description;
+      cmd.Parameters.Add(description);
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public void Edit(string newDescription)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"UPDATE items SET description = @newDescription WHERE id = @searchId;";
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = _id;
+      cmd.Parameters.Add(searchId);
+      MySqlParameter description = new MySqlParameter();
+      description.ParameterName = "@newDescription";
+      description.Value = newDescription;
+      cmd.Parameters.Add(description);
+      cmd.ExecuteNonQuery();
+      _description = newDescription;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+  }
+}
